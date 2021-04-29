5284504a46bb962e54d57c8c87fd980bf2514e5e
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-04-29 11:52:16 +0530

    Create all number.py

---
+++ datastrucutre/array/all number.py
@@ -0,0 +1,16 @@
+import sys
+
+def check(arr):
+    min = sys.maxsize
+    for i in arr:
+        if i < min:
+            min = i
+    for i in arr:
+        if not i % min == 0:
+            return False
+    return True
+
+
+arr = [20,10,15,5,100,200, 201]
+
+print(check(arr))
