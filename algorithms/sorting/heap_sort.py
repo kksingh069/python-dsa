605b6de308f8bd7f078d7fe95d731d9ef02c2701
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-02 01:12:32 +0530

    Update heap_sort.py

--- algorithms/sorting/heap_sort.py
+++ algorithms/sorting/heap_sort.py
@@ -23,7 +23,6 @@ def heap_sort(nums):
     for i in range(n, -1, -1):
         heapify(nums, n, i)
 
-    # Move the root of the max heap to the end of
     for i in range(n - 1, 0, -1):
         nums[i], nums[0] = nums[0], nums[i]
         heapify(nums, i, 0)
