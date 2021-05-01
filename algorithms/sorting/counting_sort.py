bf4c07fb04950160c308783e2c737ca6e2d97e35
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-02 00:21:22 +0530

    Update counting_sort.py

--- algorithms/sorting/counting_sort.py
+++ algorithms/sorting/counting_sort.py
@@ -6,7 +6,6 @@ It works by counting the number of objects having distinct key values (kind of h
 Then doing some arithmetic to calculate the position of each object in the output sequence.
 
 Time complexity:
-O(n+k) where n is the number of elements in input array and k is the range of input.
 
 Auxiliary Space: O(n+k)
 """
