90ce494a450c9365e85185915a62cce4f94d5b63
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-01 12:08:18 +0530

    Update bfs.py

--- algorithms/graph/bfs.py
+++ algorithms/graph/bfs.py
@@ -16,22 +16,14 @@ class Graph:
   
         queue = [] 
   
-        # Mark the source node as  
-        # visited and enqueue it 
         queue.append(s) 
         visited[s] = True
   
         while queue: 
   
-            # Dequeue a vertex from  
-            # queue and print it 
             s = queue.pop(0) 
             print (s, end = " ") 
   
-            # Get all adjacent vertices of the 
-            # dequeued vertex s. If a adjacent 
-            # has not been visited, then mark it 
-            # visited and enqueue it 
             for i in self.graph[s]: 
                 if visited[i] == False: 
                     queue.append(i) 
