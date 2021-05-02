4a875d3ceaf5a5e92c744e54f78d0ba614379b14
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-03 02:08:17 +0530

    Update random_matrix.py

--- datastrucutre/array/random_matrix.py
+++ datastrucutre/array/random_matrix.py
@@ -8,10 +8,8 @@ def random_matrix(n):
 
     m = [a[i:] + a[:i] for i in range(n + 1)]
 
-    # Shuffle rows in matrix
     shuffle(m)
 
-    # Shuffle cols in matrix (optional)
     m = list(map(list, zip(*m)))  # Transpose the matrix
     shuffle(m)
 
