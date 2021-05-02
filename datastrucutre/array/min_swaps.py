ed9eec463c3355dcfcf700a22ed27e9860190d0c
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-03 02:00:43 +0530

    Update min_swaps.py

--- datastrucutre/array/min_swaps.py
+++ datastrucutre/array/min_swaps.py
@@ -5,7 +5,6 @@ def min_swaps(arr, k):
             count += 1
 
     # This count defines a window - inside this window all our elements should 
-    # our starting answer as we will have to swap them out
     bad = 0
     for i in range(0, count):
         if arr[i] > k:
