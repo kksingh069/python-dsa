61817b32b493a19253e711f2bcc11a6fe0b6cb94
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-03 01:42:11 +0530

    Update first_repeating_char.py

--- datastrucutre/array/first_repeating_char.py
+++ datastrucutre/array/first_repeating_char.py
@@ -1,6 +1,5 @@
 # Find the first character in a string without using extra space
 # With extra space its simple. Just check for the element in a hash map
-# If present, then it is the recurrent char
 
 # Without extra space idea -
 
