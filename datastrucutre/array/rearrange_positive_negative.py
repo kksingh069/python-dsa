b001bc8c219ffa1f2d3bd3ea1b1a65977bdeb5f2
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-03 02:09:25 +0530

    Update rearrange_positive_negative.py

--- datastrucutre/array/rearrange_positive_negative.py
+++ datastrucutre/array/rearrange_positive_negative.py
@@ -8,7 +8,7 @@ def rearrange(arr):
             arr[j], arr[i] = arr[i], arr[j]
 
     pos = i + 1  # index of first positive number
-    neg = 0  # index of first negative number
+    neg = 0  
 
     while pos < len(arr) and neg < pos and arr[neg] < 0:
         arr[pos], arr[neg] = arr[neg], arr[pos]
