f9d511b27c3299b6cf8aae45af00fa01c73fa2cc
abhishektyagi2912 <abhishektyagi2912@gmail.com>
2021-05-04 01:47:01 +0530

    Update evaluate_expresssion_tree.py

--- datastrucutre/binary_tree/evaluate_expresssion_tree.py
+++ datastrucutre/binary_tree/evaluate_expresssion_tree.py
@@ -11,17 +11,13 @@ def evaluate(root):
     if root is None:
         return 0
 
-    # Check for leaf node
     if root.left is None and root.right is None:
         return int(root.data)
 
-    # Evaluate left tree
     left_sum = evaluate(root.left)
 
-    # Evaluate right tree
     right_sum = evaluate(root.right)
 
-    # Check which operator to apply
     if root.data == '+':
         return left_sum + right_sum
 
