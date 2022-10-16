d6bafe8e750737cc0edfc84bb89abb396e4f4e8c
saatvik srivastava <74640208+saatvik25@users.noreply.github.com>
2022-10-16 14:10:22 +0530

    list

---
+++ datastrucutre/list.py
@@ -0,0 +1,25 @@
+
+
+List = ["car" , "bus" , "iphone"]
+print("\nList containing multiple values: ")
+print(List)
+
+
+
+List2 = [['saatvik', 'srivastava'], ['mac']]
+print("\nMulti-Dimensional List: ")
+print(List2)
+
+
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+
+print("Accessing element using negative index")
+	
+
+print(List[-1])
+	
+
+print(List[-3])
